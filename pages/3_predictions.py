446e2a5790849cb511c04369ddd3aff7ccbb5921
wangjialin <wj716@nyu.edu>
2024-01-31 12:10:43 -0800

    third part revised

--- pages/3_predictions.py
+++ pages/3_predictions.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("Goodreads Book Recommendation System: sale predictions")
-st.write("The third part is to do future book sale predictions. Based on historical book sale statistics, we use linear or nonlinear regression to predict future sale amounts. Of course, we can add filters to the website, to focus on different types of books.")
+st.write("The third part is to do future book sale predictions. Based on historical book sale statistics, we use linear or nonlinear regression to find the most important features contributing to the book popularity, and also use time series analysis to predict future sales. ")
 
 st.sidebar.text("Web Creator: Jialin Wang")
